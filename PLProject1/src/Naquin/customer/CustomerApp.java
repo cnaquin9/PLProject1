47fa0e5d93eff8c3495df1cc3b484b028714117c
Samantha Huang <samanthahuang15@gmail.com>
2016-02-02 21:27:01 -0600

    Created Customer.java class, kind of done with CustomerApp.Java.
    Created the three customers in CustomerCB.java

--- PLProject1/src/Naquin/customer/CustomerApp.java
+++ PLProject1/src/Naquin/customer/CustomerApp.java
@@ -1,10 +1,33 @@
 package Naquin.customer;
 
+import java.util.Scanner;
+
+
 public class CustomerApp {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World!");
+
+
+
+        // Prints welcome message.
+        System.out.println("Welcome to the Customer Viewer");
+        System.out.println("");
+
+
+        Scanner user_input = new Scanner(System.in);
+
+        // Get customer number
+        String customer_number;
+        System.out.print("Enter a customer number: ");
+        customer_number = user_input.next();
+
+
+        // Check if customer exists
+        // If they exist, return customer information
+        // else, print error message
+
+        // Ask user if they would like to check another customer
+
     }
 
 }
---
+++ PLProject1/src/Naquin/customer/CustomerDB.java
@@ -0,0 +1,14 @@
+package Naquin.customer;
+
+public class CustomerDB {
+
+    //create customers
+    public static void main(String[] args) {
+        // "1001"
+        Customer c1 = new Customer("Barbara White", "3400 Richmond Parkway #3423", "Bristol", "CT", "06010");
+        // "1002"
+        Customer c2 = new Customer("Karl Vang", "327 Franklin Street", "Edina", "MN", "55435");
+        // "1003"
+        Customer c3 = new Customer("Ronda Chavan", "518 Commanche Dr.", "Greensboro", "NC", "27410");
+    }
+}
