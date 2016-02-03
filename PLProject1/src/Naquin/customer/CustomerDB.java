883cdc7e434954de1618cc5a8faabaab236de2cd
cnaquin9 <cnaquin9@yahoo.com>
2016-02-03 11:37:00 -0600

    Update CustomerDB.java

--- PLProject1/src/Naquin/customer/CustomerDB.java
+++ PLProject1/src/Naquin/customer/CustomerDB.java
@@ -6,11 +6,11 @@ public class CustomerDB {
         public static 
     Customer
     getCustomer(int customerNum) {this.customerNum = customerNum};
-    if (customerNum == 1001) {
-        customer1 = new Customer("Barbara White", "3400 Richmond Parkway #3423", "Bristol", "CT", "06010");
+    	if (customerNum == 1001) {
+        	customer1 = new Customer("Barbara White", "3400 Richmond Parkway #3423", "Bristol", "CT", "06010");
 		return String (customer1.name + “\n” + customer1.address + “\n + customer1.city + “, “ + customer1.state + “ “ + customer1.postalCode);
 	} else if (customerNum == 1002) {
-        customer2 = new Customer("Karl Vang", "327 Franklin Street", "Edina", "MN", "55435");
+        	customer2 = new Customer("Karl Vang", "327 Franklin Street", "Edina", "MN", "55435");
 		return String (customer2.name + “\n” + customer2.address + “\n + customer2.city + “, “ + customer2.state + “ “ + customer2.postalCode);
 	} else if (customerNum == 1003) {
 		customer3 = new Customer("Ronda Chavan", "518 Commanche Dr."", "Greensboro", "NC", "27410");
