d756c52070509e8cdcac14e8484eec536e1599f7
cnaquin9 <cnaquin9@yahoo.com>
2016-02-03 19:41:44 -0600

    Update CustomerDB.java

--- PLProject1/src/Naquin/customer/CustomerDB.java
+++ PLProject1/src/Naquin/customer/CustomerDB.java
@@ -3,24 +3,25 @@ package Naquin.customer;
 public class CustomerDB {
 
     //create customers
-        public static 
-    Customer
-    getCustomer(int customerNum) {this.customerNum = customerNum};
-    	if (customerNum == 1001) {
-        	customer1 = new Customer("Barbara White", "3400 Richmond Parkway #3423", "Bristol", "CT", "06010");
-		return String (customer1.name + “\n” + customer1.address + “\n + customer1.city + “, “ + customer1.state + “ “ + customer1.postalCode);
-	} else if (customerNum == 1002) {
-        	customer2 = new Customer("Karl Vang", "327 Franklin Street", "Edina", "MN", "55435");
-		return String (customer2.name + “\n” + customer2.address + “\n + customer2.city + “, “ + customer2.state + “ “ + customer2.postalCode);
-	} else if (customerNum == 1003) {
-		customer3 = new Customer("Ronda Chavan", "518 Commanche Dr."", "Greensboro", "NC", "27410");
-		return String (customer3.name + “\n” + customer3.address + “\n + customer3.city + “, “ + customer3.state + “ “ + customer3.postalCode);
-	} else {
-		return String (“There is no customer number “ + customerNum + "in our records.”);
+	private int customerNum;
+        public String getCustomer(int n) {this.customerNum = n;
+            if (customerNum == 1001) {
+                Customer customer1 = new Customer("Barbara White", "3400 Richmond Parkway #3423", "Bristol", "CT", "06010");
+                return (customer1.getNameAndAddress());
+            } else if (customerNum == 1002) {
+                Customer customer2 = new Customer("Karl Vang", "327 Franklin Street", "Edina", "MN", "55435");
+                return (customer2.getNameAndAddress());
+            } else if (customerNum == 1003) {
+                Customer customer3 = new Customer("Ronda Chavan", "518 Commanche Dr.", "Greensboro", "NC", "27410");
+                return (customer3.getNameAndAddress());
+            } else {
+                String noNum = ("There is  no customer number " + customerNum + "in our records.");
+                return noNum;}
 
-    }
+        // return customer based on int parameter.
+        // Try public static Customer (int customerNumber){}
 
-    // return customer based on int parameter.
-    // Try public static Customer (int customerNumber){}
+}}
+
 
 }
